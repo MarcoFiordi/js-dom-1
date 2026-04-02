18541fa557c7cbd162a5c1e332c559651451d40e
Marco Fiordi <marco.fiordi97@gmail.com>
2026-04-02 15:43:05 +0200

    assegnate le variabili lampadina e interruttore, e selezionati #lampadina e .interruttore con il query selector

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 'use strict'
+document.querySelector('#lampadina');
+const lampadina = document.querySelector('#lampadina');
+
+document.querySelector('.interruttore');
+const interruttore = document.querySelector('.interruttore');
